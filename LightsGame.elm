3769e4c71471f022ab0d56447a9d8a4de7f622ea
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-07-31 13:09:04 -0700

    Show a matrix of lights

--- LightsGame.elm
+++ LightsGame.elm
@@ -8,31 +8,37 @@ module LightsGame
         , view
         )
 
+import Array
 import Html
 import Html.Attributes
 import Html.Events
+import Matrix exposing (Matrix)
 
 
 type alias Model =
-    { isOn : List Bool }
+    { isOn : Matrix Bool }
 
 
-init : List Bool -> Model
+init : Matrix Bool -> Model
 init startingBoard =
     { isOn = startingBoard }
 
 
-defaultBoard : List Bool
+defaultBoard : Matrix Bool
 defaultBoard =
-    [ True, False, True, False, True ]
+    Matrix.repeat 5 5 True
 
 
 
 -- Update
 
 
+type alias LightIndex =
+    { x : Int, y : Int }
+
+
 type Msg
-    = Toggle Int
+    = Toggle LightIndex
 
 
 update : Msg -> Model -> Model
@@ -42,20 +48,14 @@ update msg model =
             { model | isOn = toggleLight indexToToggle model.isOn }
 
 
-toggleLight : Int -> List Bool -> List Bool
-toggleLight indexToToggle list =
-    List.indexedMap
-        (\i isOn ->
-            if indexToToggle == i then
-                not isOn
-            else if indexToToggle == i - 1 then
-                not isOn
-            else if indexToToggle == i + 1 then
-                not isOn
-            else
-                isOn
-        )
-        list
+toggleLight : LightIndex -> Matrix Bool -> Matrix Bool
+toggleLight indexToToggle matrix =
+    matrix
+        |> Matrix.update indexToToggle.x indexToToggle.y not
+        |> Matrix.update (indexToToggle.x + 1) indexToToggle.y not
+        |> Matrix.update (indexToToggle.x - 1) indexToToggle.y not
+        |> Matrix.update indexToToggle.x (indexToToggle.y + 1) not
+        |> Matrix.update indexToToggle.x (indexToToggle.y - 1) not
 
 
 
@@ -66,15 +66,32 @@ view : Model -> Html.Html Msg
 view model =
     Html.div []
         [ model.isOn
-            |> List.indexedMap lightButton
-            |> Html.div []
+            |> Matrix.indexedMap lightButton
+            |> matrixToDivs
         , Html.hr [] []
-        , Html.pre [] [ Html.text <| toString model ]
+        , Html.p [] [ Html.text <| toString model ]
         ]
 
 
-lightButton : Int -> Bool -> Html.Html Msg
-lightButton buttonIndex isOn =
+matrixToDivs : Matrix (Html.Html Msg) -> Html.Html Msg
+matrixToDivs matrix =
+    let
+        makeRow y =
+            Matrix.getRow y matrix
+                |> Maybe.map (Array.toList)
+                |> Maybe.withDefault []
+                |> Html.div []
+
+        height =
+            Matrix.height matrix
+    in
+        [0..height]
+            |> List.map makeRow
+            |> Html.div []
+
+
+lightButton : Int -> Int -> Bool -> Html.Html Msg
+lightButton x y isOn =
     Html.div
         [ Html.Attributes.style
             [ ( "background-color"
@@ -83,12 +100,12 @@ lightButton buttonIndex isOn =
                 else
                     "grey"
               )
-            , ( "width", "80px" )
-            , ( "height", "80px" )
+            , ( "width", "40px" )
+            , ( "height", "40px" )
             , ( "border-radius", "4px" )
             , ( "margin", "2px" )
             , ( "display", "inline-block" )
             ]
-        , Html.Events.onClick (Toggle buttonIndex)
+        , Html.Events.onClick (Toggle { x = x, y = y })
         ]
         []
--- Tests.elm
+++ Tests.elm
@@ -2,27 +2,72 @@ module Main exposing (..)
 
 import ElmTest exposing (suite, test, assertEqual)
 import LightsGame
+import Matrix
+
+
+singleLightBoard =
+    Matrix.fromList [ [ True ] ]
+        |> Maybe.withDefault Matrix.empty
+
+
+singleRowBoard =
+    Matrix.fromList [ [ False, False, False ] ]
+        |> Maybe.withDefault Matrix.empty
+
+
+squareBoard =
+    Matrix.fromList
+        [ [ False, False, False ]
+        , [ False, False, False ]
+        , [ False, False, False ]
+        ]
+        |> Maybe.withDefault Matrix.empty
 
 
 all =
     suite "LightsGame"
         [ test "can toggle a light"
-            (LightsGame.init [ True ]
-                |> LightsGame.update (LightsGame.Toggle 0)
+            (LightsGame.init singleLightBoard
+                |> LightsGame.update (LightsGame.Toggle { x = 0, y = 0 })
                 |> .isOn
-                |> assertEqual [ False ]
+                |> Matrix.get 0 0
+                |> assertEqual (Just False)
             )
         , test "toggling a light toggles its right neighbor"
-            (LightsGame.init [ False, False, False ]
-                |> LightsGame.update (LightsGame.Toggle 0)
+            (LightsGame.init singleRowBoard
+                |> LightsGame.update (LightsGame.Toggle { x = 0, y = 0 })
                 |> .isOn
-                |> assertEqual [ True, True, False ]
+                |> Matrix.get 1 0
+                |> assertEqual (Just True)
             )
         , test "toggling a light toggles its left neighbor"
-            (LightsGame.init [ False, False, False ]
-                |> LightsGame.update (LightsGame.Toggle 2)
+            (LightsGame.init singleRowBoard
+                |> LightsGame.update (LightsGame.Toggle { x = 2, y = 0 })
                 |> .isOn
-                |> assertEqual [ False, True, True ]
+                |> Matrix.get 1 0
+                |> assertEqual (Just True)
+            )
+        , test "toggling a light doesn't toggle non-neighbors"
+            (LightsGame.init squareBoard
+                |> LightsGame.update (LightsGame.Toggle { x = 1, y = 1 })
+                |> .isOn
+                |> Matrix.get 0 0
+                |> assertEqual (Just False)
+            )
+        , test
+            "toggling a light toggles its upper neighbor"
+            (LightsGame.init squareBoard
+                |> LightsGame.update (LightsGame.Toggle { x = 1, y = 2 })
+                |> .isOn
+                |> Matrix.get 1 1
+                |> assertEqual (Just True)
+            )
+        , test "toggling a light toggles its lower neighbor"
+            (LightsGame.init squareBoard
+                |> LightsGame.update (LightsGame.Toggle { x = 1, y = 0 })
+                |> .isOn
+                |> Matrix.get 1 1
+                |> assertEqual (Just True)
             )
         ]
 
