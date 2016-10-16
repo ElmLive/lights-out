a09ffad1199d015cc3389fcdd06ea82034f010d7
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-10-16 12:58:17 -0700

    Upgrade to elm-test 2.1

--- Tests.elm
+++ Tests.elm
@@ -1,8 +1,10 @@
 module Main exposing (..)
 
-import ElmTest exposing (suite, test, assertEqual)
+import Test exposing (describe, test)
+import Expect
 import LightsGame
 import Matrix
+import Test.Runner.Html
 
 
 singleLightBoard =
@@ -25,65 +27,64 @@ squareBoard =
 
 
 all =
-    suite "LightsGame"
-        [ test "can toggle a light"
-            (LightsGame.init singleLightBoard
-                |> LightsGame.update (LightsGame.Toggle { x = 0, y = 0 })
-                |> .isOn
-                |> Matrix.get 0 0
-                |> assertEqual (Just <| Just False)
-            )
-        , test "toggling a light toggles its right neighbor"
-            (LightsGame.init singleRowBoard
-                |> LightsGame.update (LightsGame.Toggle { x = 0, y = 0 })
-                |> .isOn
-                |> Matrix.get 1 0
-                |> assertEqual (Just <| Just True)
-            )
-        , test "toggling a light toggles its left neighbor"
-            (LightsGame.init singleRowBoard
-                |> LightsGame.update (LightsGame.Toggle { x = 2, y = 0 })
-                |> .isOn
-                |> Matrix.get 1 0
-                |> assertEqual (Just <| Just True)
-            )
-        , test "toggling a light doesn't toggle non-neighbors"
-            (LightsGame.init squareBoard
-                |> LightsGame.update (LightsGame.Toggle { x = 1, y = 1 })
-                |> .isOn
-                |> Matrix.get 0 0
-                |> assertEqual (Just <| Just False)
-            )
-        , test
-            "toggling a light toggles its upper neighbor"
-            (LightsGame.init squareBoard
-                |> LightsGame.update (LightsGame.Toggle { x = 1, y = 2 })
-                |> .isOn
-                |> Matrix.get 1 1
-                |> assertEqual (Just <| Just True)
-            )
-        , test "toggling a light toggles its lower neighbor"
-            (LightsGame.init squareBoard
-                |> LightsGame.update (LightsGame.Toggle { x = 1, y = 0 })
-                |> .isOn
-                |> Matrix.get 1 1
-                |> assertEqual (Just <| Just True)
-            )
-        , suite "isSolved"
-            [ test "True when all lights are off"
-                (LightsGame.init squareBoard
-                    |> LightsGame.isSolved
-                    |> assertEqual True
-                )
-            , test "False when some lights are on"
-                (LightsGame.init squareBoard
+    describe "LightsGame"
+        [ test "can toggle a light" <|
+            \() ->
+                LightsGame.init singleLightBoard
                     |> LightsGame.update (LightsGame.Toggle { x = 0, y = 0 })
-                    |> LightsGame.isSolved
-                    |> assertEqual False
-                )
+                    |> .isOn
+                    |> Matrix.get 0 0
+                    |> Expect.equal (Just <| Just False)
+        , test "toggling a light toggles its right neighbor" <|
+            \() ->
+                LightsGame.init singleRowBoard
+                    |> LightsGame.update (LightsGame.Toggle { x = 0, y = 0 })
+                    |> .isOn
+                    |> Matrix.get 1 0
+                    |> Expect.equal (Just <| Just True)
+        , test "toggling a light toggles its left neighbor" <|
+            \() ->
+                LightsGame.init singleRowBoard
+                    |> LightsGame.update (LightsGame.Toggle { x = 2, y = 0 })
+                    |> .isOn
+                    |> Matrix.get 1 0
+                    |> Expect.equal (Just <| Just True)
+        , test "toggling a light doesn't toggle non-neighbors" <|
+            \() ->
+                LightsGame.init squareBoard
+                    |> LightsGame.update (LightsGame.Toggle { x = 1, y = 1 })
+                    |> .isOn
+                    |> Matrix.get 0 0
+                    |> Expect.equal (Just <| Just False)
+        , test "toggling a light toggles its upper neighbor" <|
+            \() ->
+                LightsGame.init squareBoard
+                    |> LightsGame.update (LightsGame.Toggle { x = 1, y = 2 })
+                    |> .isOn
+                    |> Matrix.get 1 1
+                    |> Expect.equal (Just <| Just True)
+        , test "toggling a light toggles its lower neighbor" <|
+            \() ->
+                LightsGame.init squareBoard
+                    |> LightsGame.update (LightsGame.Toggle { x = 1, y = 0 })
+                    |> .isOn
+                    |> Matrix.get 1 1
+                    |> Expect.equal (Just <| Just True)
+        , describe "isSolved"
+            [ test "True when all lights are off" <|
+                \() ->
+                    LightsGame.init squareBoard
+                        |> LightsGame.isSolved
+                        |> Expect.equal True
+            , test "False when some lights are on" <|
+                \() ->
+                    LightsGame.init squareBoard
+                        |> LightsGame.update (LightsGame.Toggle { x = 0, y = 0 })
+                        |> LightsGame.isSolved
+                        |> Expect.equal False
             ]
         ]
 
 
 main =
-    ElmTest.runSuiteHtml all
+    Test.Runner.Html.run all
